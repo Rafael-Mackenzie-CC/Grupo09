61646cd32c968b9ed31d2ac865ca366a61962790
Rafael <94480697+Rafael-Mackenzie-CC@users.noreply.github.com>
2023-03-09 08:41:20 -0300

    Update exercicio1.c

--- exercicio1.c
+++ exercicio1.c
@@ -2,21 +2,21 @@
 
 int main(int argc, char *argv[]) {
     FILE *fp;
-    char line[100];
+    char linha[100];
 
     if (argc < 2) {
-        printf("Usage: %s <filename>\n", argv[0]);
+        printf("Uso: %s <filename>\n", argv[0]);
         return 1;
     }
 
     fp = fopen(argv[1], "r");
     if (fp == NULL) {
-        printf("Error opening file %s\n", argv[1]);
+        printf("Erro ao abrir o arquivo %s\n", argv[1]);
         return 1;
     }
 
-    while (fgets(line, sizeof(line), fp) != NULL) {
-        printf("%s", line);
+    while (fgets(linha, sizeof(linha), fp) != NULL) {
+        printf("%s", linha);
     }
 
     fclose(fp);
